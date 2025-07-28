196826d0b66ee68a853fa74133790004edcff97e
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-28 10:08:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let firstCard=3
+let secondCard=6
+
+sum=firstCard+secondCard+13
+console.log(sum)
+
+if(sum === 21){
+
+    console.log("you won balackjack")
+}
+else if(sum <= 20){
+
+    console.log("you can get a new card")
+}
+else{
+
+    console.log("you are out of the game")
+}
+
+
+
+
+
+
+
+
+
+// if else
+
+// let age=18;
+
+// if (age>=18){
+
+//     console.log("you are eligible to enter the club");
+// }
+// else{
+
+//     console.log("you are not eligible to enter the club");
+// }
+
+// =======================================
+// if(age >=18){
+
+//     console.log("you are eligible to enter the club");
+// }
+// else if( age===18){
+
+//      console.log("your are over 18 so you can join this club");
+// }
+
+// else{
+
+//     console.log("you are not eligible to enter the club");
+// }
+
+// =========================================
+
+// let personAge=100
+
+// if(personAge<100){
+
+//     console.log("not eligible");
+// }
+
+// else if( personAge > 100){
+
+//      console.log("not eligible,you already gotten one");
+// }
+// else{
+
+//     console.log("")
+// }
+
+
