735d307c9c5c07f0668789cece26a626e1d40044
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-28 12:34:14 +0530

    hj

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let firstCard=3
 let secondCard=6
-
+let card=firstCardsecondCard
 let sum=firstCard+secondCard+12
 let hasBlackJack=false
 let isAlive=true
 let message=""
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el")
+let cardEl=document.getElementById("cards-el")
 
 function startGame(){
 
@@ -34,6 +35,7 @@ else{
 }
     messageEl.textContent=message
     sumEl.textContent="Sum : " +sum
+    cardEl.textContent="Cards:" +card
 }
 
 
