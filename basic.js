053f7378a477a0d345ec66c34bfcdf064d913afa
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-28 12:07:01 +0530

    ssd

---
+++ basic.js
@@ -0,0 +1,120 @@
+let firstCard=3
+let secondCard=6
+
+let sum=firstCard+secondCard+13
+let hasBlackJack=false
+let isAlive=true
+let message=""
+let messageEl=document.getElementById("message-el")
+
+
+if(sum === 21){
+
+    message="you won balackjack"
+    console.log(message)
+    // hasBlackJack=true
+
+
+}
+else if(sum <= 20){
+
+    message="Do you want draw a new card?"
+    console.log(message)
+
+}
+else{
+
+    message="you're out of the game"
+    console.log(message)
+
+    // isAlive=false
+    // console.log("this is else stmt" ,isAlive)
+}
+messageEl.textContent=message
+
+
+
+
+
+// ================================================
+
+
+// let hasDiscountCode=true;
+
+// function name(){
+
+//     if (hasDiscountCode){
+
+//         console.log("you can get 30% offer")
+       
+//     }
+//     else{
+//         console.log("sorry you dont't have a coupon")
+//     }
+// }
+
+// name()
+// name()
+
+
+
+// if else
+
+// let age=18;
+
+// if (age>=18){
+
+//     console.log("you are eligible to enter the club");
+// }
+// else{
+
+//     console.log("you are not eligible to enter the club");
+// }
+
+// =======================================
+// if(age >=18){
+
+//     console.log("you are eligible to enter the club");
+// }
+// else if( age===18){
+
+//      console.log("your are over 18 so you can join this club");
+// }
+
+// else{
+
+//     console.log("you are not eligible to enter the club");
+// }
+
+// =========================================
+
+// let personAge=100
+
+// if(personAge<100){
+
+//     console.log("not eligible");
+// }
+
+// else if( personAge > 100){
+
+//      console.log("not eligible,you already gotten one");
+// }
+// else{
+
+//     console.log("")
+// }
+
+
+// ===================================
+
+// console.log(4===3) 
+// console.log(3===3)
+// console.log(5>6)
+// console.log(5>=5)
+// console.log(7<6)
+// console.log(7<=5)
+
+
+
+
+
--- script.js
+++ script.js
@@ -18,13 +18,13 @@ if(sum === 21){
 }
 else if(sum <= 20){
 
-    message="you can get a new card"
+    message="Do you want draw a new card?"
     console.log(message)
 
 }
 else{
 
-    message="you are out of the game"
+    message="you're out of the game"
     console.log(message)
 
     // isAlive=false
@@ -34,85 +34,6 @@ messageEl.textContent=message
 
 
 
-
-
-// ================================================
-
-
-// let hasDiscountCode=true;
-
-// function name(){
-
-//     if (hasDiscountCode){
-
-//         console.log("you can get 30% offer")
-       
-//     }
-//     else{
-//         console.log("sorry you dont't have a coupon")
-//     }
-// }
-
-// name()
-// name()
-
-
-
-// if else
-
-// let age=18;
-
-// if (age>=18){
-
-//     console.log("you are eligible to enter the club");
-// }
-// else{
-
-//     console.log("you are not eligible to enter the club");
-// }
-
-// =======================================
-// if(age >=18){
-
-//     console.log("you are eligible to enter the club");
-// }
-// else if( age===18){
-
-//      console.log("your are over 18 so you can join this club");
-// }
-
-// else{
-
-//     console.log("you are not eligible to enter the club");
-// }
-
-// =========================================
-
-// let personAge=100
-
-// if(personAge<100){
-
-//     console.log("not eligible");
-// }
-
-// else if( personAge > 100){
-
-//      console.log("not eligible,you already gotten one");
-// }
-// else{
-
-//     console.log("")
-// }
-
-
-// ===================================
-
-// console.log(4===3) 
-// console.log(3===3)
-// console.log(5>6)
-// console.log(5>=5)
-// console.log(7<6)
-// console.log(7<=5)
 
 
 
