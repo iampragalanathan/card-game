c42373d6f42c3481ae07ca15a55bd4a1b882fcf8
PRAGALANATHAN A <pragalanathan.eng@gmail.com>
2025-07-29 09:48:52 +0530

    add data

--- script.js
+++ script.js
@@ -53,5 +53,9 @@ function newCard(){
 }
 
 let data=["orange","apple",true,false,123]
-
-
+console.log(data)
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+console.log(data[4])
